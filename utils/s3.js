8ed3259eeae3486e60a47598e9832beda1d04aab
Jamoy Tulloch <tullochojamoy@gmail.com>
2023-01-03 15:27:02 -0500

    Update MongoDB

--- utils/s3.js
+++ utils/s3.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
 const path = require('path');
 
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const bucketName = process.env.AWS_BUCKET_NAME_CM
+const region = process.env.AWS_BUCKET_REGION_CM
+const accessKeyId = process.env.AWS_ACCESS_KEY_CM
+const secretAccessKey = process.env.AWS_SECRET_KEY_CM
 
 const s3 = new S3({
 	region,
